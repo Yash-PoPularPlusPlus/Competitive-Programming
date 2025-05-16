2ffed229b26e1f4b03528264b9a846b4bab11f18
Yash Thakker <87691410+Yash-PoPularPlusPlus@users.noreply.github.com>
2025-05-16 22:13:47 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb(x) push_back(x)
+#define mp(x,y) make_pair(x,y)
+#define all(x) x.begin(),x.end()
+#define vf first 
+#define vs second
+const int mod = 1000000007;
+ 
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+void solve(){
+	
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int t;cin >> t;
+	while(t--)
+		solve();
+}
